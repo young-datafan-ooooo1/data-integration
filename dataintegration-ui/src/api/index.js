c4f0beaa2b42d62d8703e304c0503824d5fe5da2
niuben <616652794@qq.com>
2022-03-17 17:36:58 +0800

    项目、模型、分组、webScoket地址修改

--- dataintegration-ui/src/api/index.js
+++ dataintegration-ui/src/api/index.js
@@ -1,8 +1,8 @@
+/*
+ * @Description: 
+ * @Date: 2022-03-15 12:01:57
+ */
 import * as api from './api';
 import Api from './api';
 //import { Notification } from 'element-ui';
 export default api;
-export async function getRefreshToken(reqData) {
-  let res = await Api.refreshToken(reqData);
-  return res;
-}
