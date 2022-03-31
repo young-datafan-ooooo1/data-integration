ebae27b3f9fcc27a170b38474b4e9aedd381c991
song xiaolang <songxiaolang@senses-ai.com>
2022-03-31 20:17:13 +0800

    【add】docker-centos7.6-openoffice

--- install/install.sh
+++ install/install.sh
@@ -4,7 +4,6 @@ hostIp=$1
 cd ../ && mvn clean package -DskipTests -Dcheckstyle.skip=true
 #构建镜像
 docker build -t dataintegration-group-provider dataintegration-group
-docker build -t dataintegration-file-management-provider dataintegration-file-management
 docker build -t dataintegration-gateway dataintegration-gateway
 docker build -t dataintegration-model-management-provider dataintegration-model
 docker build -t dataintegration-project-provider dataintegration-project
@@ -12,6 +11,8 @@ docker build -t dataintegration-run-management-provider dataintegration-run
 docker build -t dataintegration-sso-provider dataintegration-sso
 docker build -t dataintegration-sys-management-provider dataintegration-sys
 docker build -t dataintegration-ui dataintegration-ui
+docker build -t centos7.6-openoffice ./install/openoffice
+docker build -t dataintegration-file-management-provider dataintegration-file-management
 #启动应用
 cd install && sed -i "s,hostIp,${hostIp},g" docker-compose.yaml
 docker-compose up -d
